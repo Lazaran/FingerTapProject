2847c9fbe9ed2a2041871d43d2f20f7203251373
Lazaran <eq.scurr@gmail.com>
2024-01-14 14:19:26 -0400

    Instead of gathering individual files, just including all of Arduino, probably going to add Adafruit_BUSIO next

--- GFXBuilding/Adafruit_GFX.h
+++ GFXBuilding/Adafruit_GFX.h
@@ -1,12 +1,12 @@
 #ifndef _ADAFRUIT_GFX_H
 #define _ADAFRUIT_GFX_H
 
-#if ARDUINO >= 100
+// #if ARDUINO >= 100
 #include "Arduino.h"
 #include "Print.h"
-#else
-#include "WProgram.h"
-#endif
+// #else
+// #include "WProgram.h"
+// #endif
 #include "gfxfont.h"
 
 #include <Adafruit_I2CDevice.h>
